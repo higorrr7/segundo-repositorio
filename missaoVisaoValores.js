3cfd9039388501b77a3f76b6d74ce5afe24e7ad9
higorrr7 <164106354+higorrr7@users.noreply.github.com>
2024-10-22 22:02:58 -0300

    Aula 22/10 2

---
+++ missaoVisaoValores.js
@@ -0,0 +1,17 @@
+criaCartao(
+  'Missão',
+  'Saiba qual é a missão de nossa empresa!',
+  'Nossa missão é ...'
+)
+
+criaCartao (
+  'Visão',
+  'Saiba qual é a visão de nossa empresa!',
+  'Nossa visão é ...'
+)
+
+criaCartao(
+ 'Valores',
+ 'Saiba qual é o os Valores de nossa empresa!',
+  'Nossos Valores  é ...'
+)
